e4d9da2e70216e38a14185e7c9bb941254646b42
Oleg Frolov <frolololeg@gmail.com>
2020-08-29 21:18:44 +0300

    Added Hover3D class

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# 3D-Hover-Effect
-3D hover effect for web
+# 3D Hover Effect for WEB
+
+Have you seen these amazing 3D hover effect on Apple TV? Now you can easily make the same effect on web page. Just like that.
+
+![Apple TV 3D Hover Effect](/cover.png)
+
+## Three simple steps
+1. [Downoload it](https://raw.githubusercontent.com/Volorf/SparklinerJS/master/sparkliner.js);
+2. Add `hover3D.js` to your page;
+3. Create a new `Hover3D()` instance and set it up.
+
+**When you did it you can get something like this:**
+
+![SparklinerJS](/sparkliner.png)
+
+🔥[Live demo](https://volorf.github.io/SparklinerJS/)
+
+## How to add hover3D.js to my page
+Put a hover3D.js link in the `<head>` section. 
+Add a script with settings above the closing `</body>` tag.
+
+```html
+<!-- A 3d-hover-effect link -->
+<script src="js/hover3D.js"></script>
+</head>
+
+<!-- A user's hover3D settings -->
+<script src="js/my-script.js"></script>
+</body>
+```
+
+## How to create 3D hover effect
+Easy and quick. I'm not kidding.
+```javascript
+// Create a HoverEffect object and pass an id/className
+// of HTML element(s) you want to create the effect to.
+let myHover3D = new Hover3D(".card");
+```
+## How to customize it
+You have a bunch of properties you can tweak to control the effect.
+```javascript
+// X-axis offset
+myHover3D.xOffset = 3
+// Y-axis offset
+myHover3D.yOffset = 3
+// Attack. How fast the effect reacts to cursor movement
+myHover3D.attack = 0.1
+// Release. How fast an element repairs its initial state
+myHover3D.release = 0.5
+```
+
+
+## Links
+[Linkedin](https://www.linkedin.com/in/oleg-frolov-6a6a4752/) | [Dribbble](https://dribbble.com/Volorf) | [Twitter](https://www.twitter.com/volorf)
+
---
+++ hover3D.js
@@ -0,0 +1,50 @@
+class Hover3D 
+{
+    constructor(id)
+    {
+        this.id = id;
+        this.xOffset = 3;
+        this.yOffset = 3;
+        this.attack = 0.1;
+        this.release = 0.5;
+        this.create();
+    }
+
+    create()
+    {
+        document.querySelectorAll(this.id).forEach(element => 
+        {
+            const rectTransform = element.getBoundingClientRect();
+
+            element.style.setProperty("transform-style", "preserve-3d");
+            
+            element.addEventListener("mouseenter", e =>
+            {
+                element.style.setProperty("transition", "transform " + this.attack + "s");
+            })
+
+            element.addEventListener("mousemove", e => 
+            {
+                let dy = e.clientY - rectTransform.top;
+                let dx = e.clientX - rectTransform.left;
+                let xRot = this.map(dx, 0, rectTransform.width, -this.xOffset, this.xOffset);
+                let yRot = this.map(dy, 0, rectTransform.height, this.yOffset, -this.yOffset);
+                let propXRot = "rotateX(" + yRot + "deg) ";
+                let propYRot = "rotateY(" + xRot + "deg)";
+
+                element.style.setProperty("transform", "perspective(500px) " + propXRot + propYRot);
+            })
+
+            element.addEventListener("mouseleave", e => 
+            {
+                element.style.setProperty("transition", "transform " + this.release + "s");
+                element.style.setProperty("transform", "perspective(500px) rotateX(0deg) rotateY(0deg)");
+            })
+        })
+    }
+    // Processing map() function
+    map(value, istart, istop, ostart, ostop) 
+    {
+        return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+    }
+}
